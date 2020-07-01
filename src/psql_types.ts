f53dbe430110de1fe6ed75a3c0933f3f7c42a298
Jayden Chan <jaydencn7@gmail.com>
2020-07-01 14:06:13 -0600

    JC: Add more numerical types

--- src/psql_types.ts
+++ src/psql_types.ts
@@ -28,26 +28,68 @@ export type Column = {
 /********************************************************/
 
 export const SmallInt: SmallIntType = {
-  key: "SmallIntType",
+  key: "SmallInt",
   sqlName: "SMALLINT",
   typeScriptName: "number",
 };
 
+export const Integer: IntegerType = {
+  key: "Integer",
+  sqlName: "INTEGER",
+  typeScriptName: "number",
+};
+
+export const BigInt: BigIntType = {
+  key: "BigInt",
+  sqlName: "BIGINT",
+  typeScriptName: "BigInt",
+};
+
+export const Real: RealType = {
+  key: "Real",
+  sqlName: "REAL",
+  typeScriptName: "number",
+};
+
+export const Double: DoubleType = {
+  key: "Double",
+  sqlName: "DOUBLE PRECISION",
+  typeScriptName: "number",
+};
+
+export const SmallSerial: SmallSerialType = {
+  key: "SmallSerial",
+  sqlName: "SMALLSERIAL",
+  typeScriptName: "number",
+};
+
+export const Serial: SerialType = {
+  key: "Serial",
+  sqlName: "SERIAL",
+  typeScriptName: "number",
+};
+
+export const BigSerial: BigSerialType = {
+  key: "BigSerial",
+  sqlName: "BIGSERIAL",
+  typeScriptName: "number",
+};
+
 export const Timestamp: TimestampType = {
-  key: "TimestampType",
+  key: "Timestamp",
   sqlName: "TIMESTAMP WITHOUT TIME ZONE",
   typeScriptName: "string",
 };
 
 export const Text: TextType = {
-  key: "TextType",
+  key: "Text",
   sqlName: "TEXT",
   typeScriptName: "string",
 };
 
 export const VarChar: (length: number) => VarCharType = (length: number) => {
   return {
-    key: "VarCharType",
+    key: "VarChar",
     sqlName: "VARCHAR",
     typeScriptName: "string",
     length,
@@ -55,7 +97,7 @@ export const VarChar: (length: number) => VarCharType = (length: number) => {
 };
 
 export const Boolean: BooleanType = {
-  key: "BooleanType",
+  key: "Boolean",
   sqlName: "BOOLEAN",
   typeScriptName: "boolean",
 };
@@ -79,32 +121,74 @@ export type EnumType = UserDefinedType & {
 };
 
 export type SmallIntType = {
-  key: "SmallIntType";
+  key: "SmallInt";
   sqlName: "SMALLINT";
   typeScriptName: "number";
 };
 
+export type IntegerType = {
+  key: "Integer";
+  sqlName: "INTEGER";
+  typeScriptName: "number";
+};
+
+export type BigIntType = {
+  key: "BigInt";
+  sqlName: "BIGINT";
+  typeScriptName: "BigInt";
+};
+
+export type RealType = {
+  key: "Real";
+  sqlName: "REAL";
+  typeScriptName: "number";
+};
+
+export type DoubleType = {
+  key: "Double";
+  sqlName: "DOUBLE PRECISION";
+  typeScriptName: "number";
+};
+
+export type SmallSerialType = {
+  key: "SmallSerial";
+  sqlName: "SMALLSERIAL";
+  typeScriptName: "number";
+};
+
+export type SerialType = {
+  key: "Serial";
+  sqlName: "SERIAL";
+  typeScriptName: "number";
+};
+
+export type BigSerialType = {
+  key: "BigSerial";
+  sqlName: "BIGSERIAL";
+  typeScriptName: "number";
+};
+
 export type TimestampType = {
-  key: "TimestampType";
+  key: "Timestamp";
   sqlName: "TIMESTAMP WITHOUT TIME ZONE";
   typeScriptName: "string";
 };
 
 export type TextType = {
-  key: "TextType";
+  key: "Text";
   sqlName: "TEXT";
   typeScriptName: "string";
 };
 
 export type VarCharType = {
-  key: "VarCharType";
+  key: "VarChar";
   sqlName: "VARCHAR";
   typeScriptName: "string";
   length: number;
 };
 
 export type BooleanType = {
-  key: "BooleanType";
+  key: "Boolean";
   sqlName: "BOOLEAN";
   typeScriptName: "boolean";
 };
@@ -121,4 +205,11 @@ export type TSSQLType =
   | TextType
   | VarCharType
   | BooleanType
+  | IntegerType
+  | BigIntType
+  | RealType
+  | DoubleType
+  | SmallSerialType
+  | SerialType
+  | BigSerialType
   | EnumType;
